74b4830aa7940d764789380984a35672ea791e50
dlibberton <drew.libberton@gmail.com>
2024-02-22 16:14:25 -0500

    databases loaded and tables made

--- create_schema.sql
+++ create_schema.sql
@@ -1,9 +1,33 @@
 -- Schema
 DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS addresses;
+DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS enrollments;
 CREATE TABLE students (
   id           serial PRIMARY KEY,
   first_name   varchar(255) NOT NULL,
   last_name    varchar(255) NOT NULL,
   birthdate    date NOT NULL,
-  address_id   integer
+  address_id   integer UNIQUE
+);
+
+CREATE TABLE addresses (
+  id          INT PRIMARY KEY,
+  line_1      varchar(255),
+  city        varchar(255),
+  state       varchar(255),
+  zipcode     varchar(255)
+);
+
+CREATE TABLE classes (
+  id        serial PRIMARY KEY,
+  name      varchar(255) NOT NULL,
+  credits   integer
+);
+
+CREATE TABLE enrollments (
+  id            serial PRIMARY KEY,
+  student_id    integer NOT NULL,
+  class_id      integer NOT NULL,
+  grade         varchar(255)
 );
